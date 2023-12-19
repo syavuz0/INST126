da8baafdb21035267783931b26ca20d7841175cb
syavuz0 <syavuz@terpmail.umd.edu>
2023-12-18 20:14:35 -0500

    Cleaning Up
    Debugging and getting rid of code that is giving a type error between string and integer.

--- final /final.py
+++ final /final.py
@@ -3,31 +3,29 @@
 # using advanced topics git, pandas, reg exp
 import pandas as pd 
 import numpy as np 
-import regex as re
+import regex as re 
 import matplotlib.pyplot as plt 
 
 df = pd.read_csv('movie_data.csv')
 print(df)
 
-pattern = r'\bcomedy\b'
+pattern = r'\b[Cc]omedy\b'
 
-def filter_plot(df, genre_pattern): 
+comedy_movies = df[df['Genre'].str.contains(pattern, flags=re.IGNORECASE, na=False)]
+print("Here are all of the comedy movies:")
+print(comedy_movies[["Title", "Rating"]]) 
 
-    comedy_movies = df[df['Genre'].str.contains(genre_pattern, flags=re.IGNORECASE, na=False)]
-    print(comedy_movies)
+try:
+    min_rating = int(input("Enter the minimum rating to filter the comedy movies: "))
+except ValueError:
+    print("Invalid input, please enter in a valid numeric value.")
+    min_rating = 4.1 
 
-    print(f"{genre_pattern.capitalize()}")
+df["Rating"] = pd.to_numeric(df["Rating"], errors='coerce')
 
-    try:
-        min_rating = float(input("Enter the minimum rating to filter the comedy movies: "))
-    except ValueError:
-        print("Invalid input, please enter in a valid numeric value.")
-        min_rating = np.nan # placeholder for an invalid input 
+filtered_movies = comedy_movies[comedy_movies["Rating"] >= {min_rating}]
 
-    filtered_movies = np
+min_rating = 7 
 
-    mean_rating_comedy = np.mean(pd.to_numeric(comedy_movies['Rating'], errors='coerce'))
-    print(f"\nThe comedy movies with a rating that is greater than or equal to {min_rating}: ")
-
-
-
+print(f"\nFiltered Comedy Movies with Rating >= {min_rating}")
+print(filtered_movies[["Title", "Rating"]])
